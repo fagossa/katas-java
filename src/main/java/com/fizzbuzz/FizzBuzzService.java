6f349ba2525aecfbbb32a13b2514651de4ba9908
Fabian Gutierrez <fagossa@gmail.com>
2016-10-11 22:09:50 +0200

    remove solution for fizzbuzz

--- src/main/java/com/fizzbuzz/FizzBuzzService.java
+++ src/main/java/com/fizzbuzz/FizzBuzzService.java
@@ -2,29 +2,5 @@ package com.fizzbuzz;
 
 public class FizzBuzzService {
 
-    public static final String FIZZ = "fizz";
-    public static final String BUZZ = "buzz";
 
-    public String guess(int aNumber) {
-        if (isMultipleOf3(aNumber) && isMultipleOf5(aNumber)) {
-            return FIZZ + BUZZ;
-        }
-
-        if(isMultipleOf5(aNumber)) {
-            return BUZZ;
-        }
-
-        if (isMultipleOf3(aNumber)) {
-            return FIZZ;
-        }
-        return String.valueOf(aNumber);
-    }
-
-    private boolean isMultipleOf5(int aNumber) {
-        return aNumber % 5 == 0;
-    }
-
-    private boolean isMultipleOf3(int aNumber) {
-        return aNumber % 3 == 0;
-    }
 }
